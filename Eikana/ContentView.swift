b290441b39e9b55c41c47e9816508cdbac29e448
SaturnR7 <hidemasa.kobayashi@gmail.com>
2026-06-18 09:00:27 +0900

    Initial Commit

---
+++ Eikana/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Eikana
+//
+//  Created by Hidemasa Kobayashi on 2026/06/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Eikana/EikanaApp.swift
@@ -0,0 +1,17 @@
+//
+//  EikanaApp.swift
+//  Eikana
+//
+//  Created by Hidemasa Kobayashi on 2026/06/18.
+//
+
+import SwiftUI
+
+@main
+struct EikanaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
